4442b64da4e4358e64b4278b3bbff6d8165d18f9
Ghebrehiwet <ghhiwethh@gmail.com>
2022-05-30 20:15:34 +0300

    Modification of the files

--- App.js
+++ App.js
@@ -1,24 +1,25 @@
-const express=require('express')
-const app=express()
-const port=3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/',(req,res)=>{
-    res.send("<h1>Hello word to express demo</h1>  <h4>Message: sucesss</h4>");
-
-})
-app.get('/product',(req,res)=>{
-    res.send([
-        {
-            productId:10,
-            productName:'Laptop'
-        },
-        {
-            productId:11,
-            productName:"printer"
-        }
-    ])
+app.get('/', (req, res) => {
+  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
 })
 
-app.listen(port,(req,res)=>{
-    console.log("Listening to port:`${port}`")
+app.get('/products', (req, res) => {
+  res.send([
+    {
+      productId: '101',
+      price: 100
+    },
+    {
+      productId: '102',
+      price: 150
+    }
+  ])
 })
+
+app.listen(port, ()=> {
+  console.log(`Demo app is up and listening to port: ${port}`);
+})
+ 
